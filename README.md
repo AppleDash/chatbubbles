1ebe91b5a75372fad1b8a6531ebe6a78da215186
AppleDash <pet@feather.horse>
2022-11-03 13:34:52 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+Chat Bubbles
+============
+
+A simple mod for Minecraft to display chat bubbles above players' heads when they chat in-game.
---
+++ src/main/java/org/appledash/chatbubbles/ChatBubbles.java
@@ -0,0 +1,10 @@
+package org.appledash.chatbubbles;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChatBubbles implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
